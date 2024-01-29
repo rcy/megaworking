a94fc2cc1ba8a916bff65f48131fbfcfda0e5e7b
Ryan Yeske <ryan@ryanyeske.com>
2024-01-28 19:13:27 -0700

    Cycle timer added

---
+++ internal/cycletimer/cycletimer.go
@@ -0,0 +1,67 @@
+package cycles
+
+import "time"
+
+type CycleTimer struct {
+	workDuration  time.Duration
+	restDuration  time.Duration
+	restEndAt     time.Duration
+	workEndAt     time.Duration
+	cycleDuration time.Duration
+	origin        time.Time
+}
+
+type state uint8
+
+const (
+	Rest state = iota
+	Work
+)
+
+type Cycle struct {
+	ID        int64
+	State     state
+	Remaining time.Duration
+}
+
+var origin = time.Date(2022, time.June, 0, 1, 0, 0, 0, time.UTC)
+
+func New() CycleTimer {
+	return NewCustom(30, 10, origin)
+}
+
+func NewCustom(workMinutes, restMinutes int, origin time.Time) CycleTimer {
+	workDuration := time.Minute * time.Duration(workMinutes)
+	restDuration := time.Minute * time.Duration(restMinutes)
+
+	return CycleTimer{
+		origin:        origin,
+		workDuration:  workDuration,
+		restDuration:  restDuration,
+		restEndAt:     restDuration,
+		workEndAt:     restDuration + workDuration,
+		cycleDuration: restDuration + workDuration,
+	}
+}
+
+func (cs CycleTimer) CurrentCycle() Cycle {
+	return cs.CycleAt(time.Now())
+}
+
+func (cs CycleTimer) CycleAt(when time.Time) Cycle {
+	var cycle Cycle
+
+	elapsed := when.Sub(cs.origin)
+	pos := elapsed % cs.cycleDuration
+	cycle.ID = int64(elapsed / cs.cycleDuration)
+
+	if pos < cs.restEndAt {
+		cycle.State = Rest
+		cycle.Remaining = cs.restEndAt - pos
+	} else {
+		cycle.State = Work
+		cycle.Remaining = cs.workEndAt - pos
+	}
+
+	return cycle
+}
---
+++ internal/cycletimer/cycletimer_test.go
@@ -0,0 +1,51 @@
+package cycles
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestCycleTimer(t *testing.T) {
+	start := origin
+
+	for _, tc := range []struct {
+		when time.Time
+		want Cycle
+	}{
+		{
+			when: start,
+			want: Cycle{ID: 0, State: Rest, Remaining: 10 * time.Minute},
+		},
+		{
+			when: start.Add(10 * time.Minute),
+			want: Cycle{ID: 0, State: Work, Remaining: 30 * time.Minute},
+		},
+		{
+			when: start.Add(13 * time.Minute + 33 * time.Second),
+			want: Cycle{ID: 0, State: Work, Remaining: 26 * time.Minute + 27 * time.Second},
+		},
+		{
+			when: start.Add(20 * time.Minute),
+			want: Cycle{ID: 0, State: Work, Remaining: 20 * time.Minute},
+		},
+		{
+			when: start.Add(40 * time.Minute),
+			want: Cycle{ID: 1, State: Rest, Remaining: 10 * time.Minute},
+		},
+		{
+			when: start.Add(1033 * time.Minute),
+			want: Cycle{ID: 25, State: Work, Remaining: 7 * time.Minute},
+		},
+	} {
+		t.Run(fmt.Sprint(tc.when), func(t *testing.T) {
+			timer := New()
+
+			got := timer.CycleAt(tc.when)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("\nwant: %v\n got: %v", tc.want, got)
+			}
+		})
+	}
+}
