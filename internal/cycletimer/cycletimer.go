ea579731c764e3844b7ba8db6d8c9aecf2599ea7
Ryan Yeske <ryan@ryanyeske.com>
2024-02-03 13:16:56 -0700

    CycleTimer Tests

--- internal/cycletimer/cycletimer.go
+++ internal/cycletimer/cycletimer.go
@@ -1,6 +1,9 @@
 package cycletimer
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type CycleTimer struct {
 	workDuration  time.Duration
@@ -9,25 +12,37 @@ type CycleTimer struct {
 	workEndAt     time.Duration
 	cycleDuration time.Duration
 	origin        time.Time
+	startAt       time.Time
+	numCycles     int64
 }
 
 type Phase uint8
 
 func (p Phase) String() string {
-	if p == Rest {
+	switch p {
+	case Void:
+		return "Void"
+	case Rest:
 		return "Rest"
+	case Work:
+		return "Work"
+	case Done:
+		return "Done"
+	default:
+		panic("unknown case")
 	}
-	return "Work"
 }
 
 const (
 	Void Phase = iota
 	Rest
 	Work
+	Done
 )
 
 type Cycle struct {
 	ID             int64
+	Number         int64
 	Phase          Phase
 	PhaseDuration  time.Duration
 	PhaseRemaining time.Duration
@@ -39,11 +54,12 @@ func (c Cycle) PhasePercentComplete() float64 {
 
 var origin = time.Date(2022, time.June, 0, 1, 0, 0, 0, time.UTC)
 
-func New() CycleTimer {
-	return NewCustom(30*time.Minute, 10*time.Minute, origin)
+func New(startAt time.Time, numCycles int64) CycleTimer {
+	panic("not implemented") // this breaks tests, work this out later
+	return NewCustom(30*time.Minute, 10*time.Minute, origin, startAt, numCycles)
 }
 
-func NewCustom(workDuration, restDuration time.Duration, origin time.Time) CycleTimer {
+func NewCustom(workDuration, restDuration time.Duration, origin time.Time, startAt time.Time, numCycles int64) CycleTimer {
 	return CycleTimer{
 		origin:        origin,
 		workDuration:  workDuration,
@@ -51,11 +67,21 @@ func NewCustom(workDuration, restDuration time.Duration, origin time.Time) Cycle
 		restEndAt:     restDuration,
 		workEndAt:     restDuration + workDuration,
 		cycleDuration: restDuration + workDuration,
+		startAt:       startAt,
+		numCycles:     numCycles,
 	}
 }
 
 func (cs CycleTimer) CurrentCycle() Cycle {
 	return cs.CycleAt(time.Now())
+}
+
+func (cs CycleTimer) FirstCycle() Cycle {
+	return cs.CycleAt(cs.startAt)
+}
+
+func (cs CycleTimer) NumCycles() int64 {
+	return cs.numCycles
 }
 
 type Ticker struct {
@@ -83,6 +109,16 @@ func (cs CycleTimer) NewTicker(interval time.Duration) *Ticker {
 	return t
 }
 
+func (cs CycleTimer) cycleIDAt(when time.Time) int64 {
+	if cs.cycleDuration == 0 {
+		return 0
+	}
+
+	elapsed := when.Sub(cs.origin)
+
+	return int64(elapsed / cs.cycleDuration)
+}
+
 func (cs CycleTimer) CycleAt(when time.Time) Cycle {
 	cycle := Cycle{}
 
@@ -92,17 +128,28 @@ func (cs CycleTimer) CycleAt(when time.Time) Cycle {
 
 	elapsed := when.Sub(cs.origin)
 	pos := elapsed % cs.cycleDuration
-	cycle.ID = int64(elapsed / cs.cycleDuration)
+	cycle.ID = cs.cycleIDAt(when)
+	startingID := cs.cycleIDAt(cs.startAt)
+	cycleNumber := cycle.ID - startingID + 1
+
+	cycle.Number = cycleNumber
+	log.Printf("cycleNumber %d\n", cycleNumber)
+
+	if cycleNumber > cs.numCycles {
+		cycle.Phase = Done
+		cycle.Number = cs.numCycles
+		return cycle
+	}
 
 	if pos < cs.restEndAt {
 		cycle.Phase = Rest
 		cycle.PhaseRemaining = cs.restEndAt - pos
 		cycle.PhaseDuration = cs.restDuration
-	} else {
-		cycle.Phase = Work
-		cycle.PhaseRemaining = cs.workEndAt - pos
-		cycle.PhaseDuration = cs.workDuration
+		return cycle
 	}
 
+	cycle.Phase = Work
+	cycle.PhaseRemaining = cs.workEndAt - pos
+	cycle.PhaseDuration = cs.workDuration
 	return cycle
 }
--- internal/cycletimer/cycletimer_test.go
+++ internal/cycletimer/cycletimer_test.go
@@ -7,51 +7,69 @@ import (
 	"time"
 )
 
-func TestCycleTimer(t *testing.T) {
-	start := origin
+func TestCycleTimerCustom(t *testing.T) {
+	start := time.Now()
+	//start := origin
 
 	for _, tc := range []struct {
+		name string
 		when time.Time
 		want Cycle
 	}{
 		{
+			name: "start",
 			when: start,
-			want: Cycle{ID: 0, Phase: Rest, PhaseRemaining: 10 * time.Minute},
+			want: Cycle{ID: 0, Number: 1, Phase: Rest, PhaseDuration: 10 * time.Minute, PhaseRemaining: 10 * time.Minute},
 		},
 		{
+			name: "10 minutes in",
 			when: start.Add(10 * time.Minute),
-			want: Cycle{ID: 0, Phase: Work, PhaseRemaining: 30 * time.Minute},
+			want: Cycle{ID: 0, Number: 1, Phase: Work, PhaseDuration: 30 * time.Minute, PhaseRemaining: 30 * time.Minute},
 		},
 		{
+			name: "13 minutes in",
 			when: start.Add(13*time.Minute + 33*time.Second),
-			want: Cycle{ID: 0, Phase: Work, PhaseRemaining: 26*time.Minute + 27*time.Second},
+			want: Cycle{ID: 0, Number: 1, Phase: Work, PhaseDuration: 30 * time.Minute, PhaseRemaining: 26*time.Minute + 27*time.Second},
 		},
 		{
+			name: "20 minutes in",
 			when: start.Add(20 * time.Minute),
-			want: Cycle{ID: 0, Phase: Work, PhaseRemaining: 20 * time.Minute},
+			want: Cycle{ID: 0, Number: 1, Phase: Work, PhaseDuration: 30 * time.Minute, PhaseRemaining: 20 * time.Minute},
 		},
 		{
+			name: "40 minutes in",
 			when: start.Add(40 * time.Minute),
-			want: Cycle{ID: 1, Phase: Rest, PhaseRemaining: 10 * time.Minute},
+			want: Cycle{ID: 1, Number: 2, Phase: Rest, PhaseDuration: 10 * time.Minute, PhaseRemaining: 10 * time.Minute},
 		},
 		{
+			name: "way in",
 			when: start.Add(1033 * time.Minute),
-			want: Cycle{ID: 25, Phase: Work, PhaseRemaining: 7 * time.Minute},
+			want: Cycle{ID: 25, Number: 6, Phase: Done},
+		},
+		{
+			name: "Done",
+			when: start.Add(4 * time.Hour),
+			want: Cycle{ID: 6, Number: 6, Phase: Done},
 		},
 	} {
-		t.Run(fmt.Sprint(tc.when), func(t *testing.T) {
-			timer := New()
+		t.Run(tc.name, func(t *testing.T) {
+			timer := NewCustom(30*time.Minute, 10*time.Minute, start, start, 6)
 
 			got := timer.CycleAt(tc.when)
+
+			got.ID = 0
+			tc.want.ID = 0
+
 			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("\nwant: %v\n got: %v", tc.want, got)
+				t.Errorf("\nwant: %+v\n got: %+v", tc.want, got)
 			}
 		})
 	}
 }
 
 func TestTicker(t *testing.T) {
-	timer := New()
+	start := time.Now()
+	timer := NewCustom(30*time.Minute, 10*time.Minute, start, start, 6)
 	ticker := timer.NewTicker(time.Millisecond * 100)
 
 	counter := 0
